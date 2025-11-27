70a0955ca2681ff30a85fff3969266650da06230
x5pid <x5pid.series@gmail.com>
2025-11-27 23:21:26 +0100

    1

--- frontend/src/app/feature/home/home.component.ts
+++ frontend/src/app/feature/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from "@angular/core";
 import { Countdown } from "../countdown/countdown";
+import { MatButton } from "@angular/material/button";
 
 @Component({
   selector: 'app-home',
   imports: [
-    Countdown
+    Countdown,
+    MatButton
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
