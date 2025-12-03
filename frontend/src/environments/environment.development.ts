3fff5b06ffb8816bc9d15b3912485f49d0482065
x5pid <x5pid.series@gmail.com>
2025-12-03 12:11:46 +0100

    ok

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://127.0.0.1:8000/api/v1'
-  apiUrl: 'https://betbb-production.up.railway.app/api/v1'
+  apiUrl: 'http://127.0.0.1:8000/api/v1'
+  //apiUrl: 'https://betbb-production.up.railway.app/api/v1'
 };
