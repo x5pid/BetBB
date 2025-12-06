af5ca5f9a86d4c601c0a127a74eb09b42c066434
x5pid <x5pid.series@gmail.com>
2025-12-06 15:21:23 +0100

    ok

--- frontend/src/app/feature/bet/bet-result/bet-result.ts
+++ frontend/src/app/feature/bet/bet-result/bet-result.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { BetService } from '../../../core/services/bet.service';
 
 @Component({
   selector: 'app-bet-rules-dialog',
@@ -11,6 +12,18 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class BetResult {
   constructor(public dialogRef: MatDialogRef<BetResult>) {}
+  private _serviceBet = inject(BetService);
+
+  // User Bet Stats
+  private _userBetStats = this._serviceBet.userBetStats?.data;
+  userBetStatsSuccess = this._serviceBet.userBetStats?.success;
+  userBetStatsLoading = this._serviceBet.userBetStats?.loading;
+
+  userGenderGirl = computed(() => {
+    const stats = this._userBetStats();
+    if (!stats) return false;
+    return stats.gender == "Fille";
+  });
 
   close(): void {
     this.dialogRef.close();
